1ecc827abb3a353bb4fd0f081f582e1d3121546b
Kenni Gandira Alamsyah <devkenni.g@gmail.com>
2025-09-29 15:06:28 +0700

    fix: reactivate homepage placeholder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import InfiniteStopWar from '@/components/misc/InfiniteStopWar'
 export default function HomePage() {
   return (
     <div>
-      {/* <InfiniteStopWar /> */}
-      Hot Threads
+      <InfiniteStopWar />
+      {/* Hot Threads */}
     </div>
   )
 }
